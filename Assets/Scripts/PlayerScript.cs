33058981691fb288b921e1ac496c0d388a1d46d5
Eric Freeman <eric.freeman@extendhealth.com>
2014-04-22 07:11:56 -0600

    some temp images and movement for a shmup

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float MaxSpeed = 5f;
+    private float _currentSpeed;
+
+    public float MinX = -8;
+    public float MinY = -3;
+    public float MaxX = -2;
+    public float MaxY = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        _currentSpeed = MaxSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var xSpd = Input.GetAxisRaw("Horizontal");
+        var ySpd = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(new Vector3(xSpd, ySpd, 0) * _currentSpeed * Time.deltaTime);
+        if (transform.position.x < MinX) transform.Translate(MinX - transform.position.x, 0, 0);
+        if (transform.position.x > MaxX) transform.Translate(MaxX - transform.position.x, 0, 0);
+        if (transform.position.y < MinY) transform.Translate(0, MinY - transform.position.y, 0);
+        if (transform.position.y > MaxY) transform.Translate(0, MaxY - transform.position.y, 0);
+    }
+}
