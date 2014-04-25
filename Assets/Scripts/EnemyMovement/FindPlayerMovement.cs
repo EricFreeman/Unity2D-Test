2f23038b4447d0d3fcbaaed070e21390f61d7be1
Eric Freeman <eric.freeman@extendhealth.com>
2014-04-25 08:07:18 -0600

    Added new type of enemy and movement.  This guy will just try to divebomb the player

---
+++ Assets/Scripts/EnemyMovement/FindPlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.EnemyMovement
+{
+    public class FindPlayerMovement : MonoBehaviour, IMovement
+    {
+        public float Speed { get; set; }
+
+        void Update()
+        {
+            var p = GameObject.FindGameObjectWithTag("Player");
+            if (p != null)
+               transform.Translate(
+                   Vector3.MoveTowards(
+                        transform.position,
+                        p.transform.position, 
+                        Speed)); 
+            else
+                transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+    }
+}
