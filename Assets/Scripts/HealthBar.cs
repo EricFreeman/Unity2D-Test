c45bc82062d8234071dfcdf12a18e3a42b0eaba1
Eric Freeman <eric.freeman@extendhealth.com>
2014-04-29 08:10:00 -0600

    Health bar works

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var player = GameObject.Find("Player");
+        if (player != null)
+        {
+            var health = player.GetComponent<Health>();
+            guiTexture.pixelInset = new Rect(0, 0, health.CurrentHealth/health.MaxHealth * 128, 8);
+        }
+    }
+}
