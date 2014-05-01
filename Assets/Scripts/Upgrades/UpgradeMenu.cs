26bd7b6b509c4e6d378806dbffc750b4d6f7ce3e
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-01 07:33:50 -0600

    Added way to switch what category of items you want to see and clicking buttons will subtract that amount of money from you

--- Assets/Scripts/Upgrades/UpgradeMenu.cs
+++ Assets/Scripts/Upgrades/UpgradeMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class UpgradeMenu : MonoBehaviour
 {
-    private float _money = 0;
+    private float _money = 300;
     private StoreCategory _selectedCategory = StoreCategory.Weapon;
     private List<StoreItem> _storeItems = new List<StoreItem>();
 
@@ -14,10 +14,13 @@ public class UpgradeMenu : MonoBehaviour
 
     void Start()
     {
-        // load a bunch of temp store items
         _storeItems.Add(new StoreItem("Gun1", "Starter gun", 5, StoreCategory.Weapon));
         _storeItems.Add(new StoreItem("Gun2", "Okay gun", 25, StoreCategory.Weapon));
         _storeItems.Add(new StoreItem("Gun3", "Best gun", 250, StoreCategory.Weapon));
+
+        _storeItems.Add(new StoreItem("Armor1", "Starter armor", 5, StoreCategory.ShipUpgrade));
+        _storeItems.Add(new StoreItem("Armor2", "Okay armor", 25, StoreCategory.ShipUpgrade));
+        _storeItems.Add(new StoreItem("Armor3", "Best armor", 250, StoreCategory.ShipUpgrade));
     }
 
     void OnGUI()
@@ -29,17 +32,27 @@ public class UpgradeMenu : MonoBehaviour
 
         GUI.Box(new Rect(0, 0, guiWidth, guiHeight), "Buy");
 
-        _scrollPosition = GUI.BeginScrollView(
-            new Rect(0, 20, guiWidth, guiHeight - 20),      // pos and size of viewer
-            _scrollPosition,                                // current scroll pos
-            new Rect(0, 0, guiWidth - 20, currentItems.Count() * lineHeight)); // size of section you can scroll
+        if (GUI.Button(new Rect(10, 20, 100, 30), "Weapon"))
+            _selectedCategory = StoreCategory.Weapon;
+        if (GUI.Button(new Rect(110, 20, 100, 30), "Ship Upgrade"))
+            _selectedCategory = StoreCategory.ShipUpgrade;
+        if (GUI.Button(new Rect(210, 20, 100, 30), "Ship"))
+            _selectedCategory = StoreCategory.Ship;
 
-        var currentY = 0;
+        _scrollPosition = GUI.BeginScrollView(
+            new Rect(0, 20, guiWidth, guiHeight - 20),                                      // pos and size of viewer
+            _scrollPosition,                                                                // current scroll pos
+            new Rect(0, 0, guiWidth - 20, currentItems.Count() * lineHeight + lineHeight)); // size of section you can scroll
+
+        var currentY = 30;
         foreach (var storeItem in currentItems)
         {
             if (GUI.Button(new Rect(10, currentY, 100, 30), "{0} - {1}".ToFormat(storeItem.Name, storeItem.Price)))
             {
-                //check if you have enough money, then add damage to player ship or something?
+                if (_money > storeItem.Price)
+                {
+                    _money -= storeItem.Price;
+                }
             }
 
             currentY += lineHeight;
