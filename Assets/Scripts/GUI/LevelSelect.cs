27cb398309598b022d1e53d2a5c25b946f373de2
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-05 17:59:58 -0600

    Added level select scene

---
+++ Assets/Scripts/GUI/LevelSelect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+    public string LevelName;
+
+    void Start()
+    {
+        var label = GetComponentInChildren<UILabel>();
+
+        if(label != null)
+            label.text = LevelName;
+    }
+
+    void OnCLick()
+    {
+        PlayerPrefs.SetString("SelectedLevel", LevelName);
+    }
+}
