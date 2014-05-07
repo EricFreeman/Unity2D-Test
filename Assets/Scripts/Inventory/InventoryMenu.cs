00bcd072010fc05185774924c78cff13ca919a46
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-07 07:52:01 -0600

    Start of inventory screen so you can add items and stuff to your ship

---
+++ Assets/Scripts/Inventory/InventoryMenu.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Models;
+using Assets.Scripts.Upgrades;
+using Assets.Services;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour
+{
+    private PlayerModel _currentPlayer;
+
+    void Start()
+    {
+        var manager = new XmlManager<PlayerModel>();
+        _currentPlayer = manager.Load("savegame1.xml");
+    }
+
+    public void Equip(Item item)
+    {
+        _currentPlayer.Inventory.Remove(item);
+        _currentPlayer.EquippedItems.Add(item);
+    }
+
+    public void UnEquip(Item item)
+    {
+        _currentPlayer.EquippedItems.Remove(item);
+        _currentPlayer.Inventory.Add(item);
+    }
+}
