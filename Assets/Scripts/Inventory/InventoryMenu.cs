94917834cd3dc18031f2eef6f49205bb16d2ab7d
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-07 17:18:48 -0600

    Load buttons in scroller from player's inventory and set names correctly

--- Assets/Scripts/Inventory/InventoryMenu.cs
+++ Assets/Scripts/Inventory/InventoryMenu.cs
@@ -7,10 +7,40 @@ public class InventoryMenu : MonoBehaviour
 {
     private PlayerModel _currentPlayer;
 
+    // Top Bar
+    public Transform Grid;
+
+    // Bottom Bar
+    public Transform SelectedItemLabel;
+    public Transform EquipButton;
+    public Transform UnequipButton;
+
     void Start()
     {
         var manager = new XmlManager<PlayerModel>();
         _currentPlayer = manager.Load("savegame1.xml");
+
+        ReloadInventory();
+    }
+
+    void ReloadInventory()
+    {
+        ClearInventoryGrid();
+        foreach (var inv in _currentPlayer.Inventory)
+        {
+            var i = (GameObject)Instantiate(Resources.Load("Prefabs/GUI/InventoryItem"));
+            i.transform.parent = Grid;
+            i.transform.localScale = Vector3.one;
+
+            i.GetComponentInChildren<UILabel>().text = inv.Name;
+        }
+        Grid.GetComponent<UIGrid>().repositionNow = true;
+    }
+
+    void ClearInventoryGrid()
+    {
+        foreach (Transform child in Grid.transform)
+            Destroy(child.gameObject);
     }
 
     public void Equip(Item item)
@@ -19,7 +49,7 @@ public class InventoryMenu : MonoBehaviour
         _currentPlayer.EquippedItems.Add(item);
     }
 
-    public void UnEquip(Item item)
+    public void Unequip(Item item)
     {
         _currentPlayer.EquippedItems.Remove(item);
         _currentPlayer.Inventory.Add(item);
