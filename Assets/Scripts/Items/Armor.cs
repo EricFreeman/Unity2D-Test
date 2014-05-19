e19b3706eb8b21fd669d4cde363bac4e8effc821
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-19 08:50:45 -0600

    Added an armor item that you can buy in the store that just increases your max health

---
+++ Assets/Scripts/Items/Armor.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class Armor : MonoBehaviour
+{
+    public float ArmorAmount = 10;
+
+    void Start()
+    {
+        var player = transform.parent;
+        var health = player.GetComponent<Health>();
+        if (health != null)
+        {
+            health.MaxHealth += ArmorAmount;
+            health.CurrentHealth += ArmorAmount;
+        }
+    }
+}
